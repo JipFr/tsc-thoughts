234cccc70284b58a07ab162c201601c1f40eef63
Jip Fr <jipfrijlink@gmail.com>
2020-03-31 19:24:54 +0200

    Get rid of modules, f those

--- src/classes.ts
+++ src/classes.ts
@@ -1,1 +1,46 @@
 
+class Thought {
+	public text: string;
+	public made: string;
+	public id: number;
+
+	constructor(text: string, id: number, made?: string) {
+		this.text = text;
+		this.made = made || JSON.stringify(new Date());
+		this.id = id;
+	}
+
+	public render(): void {
+		let wrapper: Element = document.querySelector(".thoughts");
+		let thoughtHtml: string = `
+		<div class='thought' data-id='${this.id}'>
+			<h3 class='thoughtTitle'>
+				<span class='id'>#${this.id.toString().padStart(3, "0")}</span>
+				<a class='delete' href='javascript:void(0)'>Delete</a>
+			</h3>
+			<p class='content'>${this.text}</p>
+		</div>
+		`;
+		let node: Element = parseHtml(thoughtHtml);
+		
+		node.querySelector(".delete")
+		  .addEventListener("click", () => {
+			if(confirm(`Delete #${this.id.toString().padStart(3, "0")}?`)) this.delete();
+		  });
+		
+		if(wrapper.children.length > 0) {
+			wrapper.insertBefore(node, wrapper.children[0]);
+		} else {
+			wrapper.appendChild(node);
+		}
+	}
+
+	public delete(): void {
+		thoughts = thoughts.filter(t => t.id !== this.id);
+		document.querySelectorAll(`[data-id="${this.id}"]`).forEach(el => el.remove());
+		setStorage();
+	}
+
+}
+
+let thoughts: Thought[] = [];
--- src/main.ts
+++ src/main.ts
@@ -1,60 +1,6 @@
 
-class Thought {
-	public text: string;
-	public made: string;
-	public id: number;
-
-	constructor(text: string, id: number, made?: string) {
-		this.text = text;
-		this.made = made || JSON.stringify(new Date());
-		this.id = id;
-	}
-
-	public render(): void {
-		let wrapper: Element = document.querySelector(".thoughts");
-		let thoughtHtml: string = `
-		<div class='thought' data-id='${this.id}'>
-			<h3 class='thoughtTitle'>
-				<span class='id'>#${this.id.toString().padStart(3, "0")}</span>
-				<a class='delete' href='javascript:void(0)'>Delete</a>
-			</h3>
-			<p class='content'>${this.text}</p>
-			<!--<a class='delete' href='javascript:void(0)'>Delete</a> <span class='id'>#${this.id.toString().padStart(3, "0")}</span> <span>${this.text}</span>-->
-		</div>
-		`;
-		let node: Element = parseHtml(thoughtHtml);
-		
-		node.querySelector(".delete")
-		  .addEventListener("click", () => {
-			if(confirm(`Delete #${this.id.toString().padStart(3, "0")}?`)) this.delete();
-		  });
-		
-		if(wrapper.children.length > 0) {
-			wrapper.insertBefore(node, wrapper.children[0]);
-		} else {
-			wrapper.appendChild(node);
-		}
-	}
-
-	public delete(): void {
-		thoughts = thoughts.filter(t => t.id !== this.id);
-		document.querySelectorAll(`[data-id="${this.id}"]`).forEach(el => el.remove());
-		setStorage();
-	}
-
-
-
-}
-let thoughts: Thought[] = [];
-
-function parseHtml(html: string): Element {
-	let div = document.createElement("div");
-	div.innerHTML = html;
-	return div.children[0];
-}
-
 function getNewId(): number {
-	let largest: number = -1;
+	let largest = -1;
 	for(let thought of thoughts) {
 		if(thought.id > largest) {
 			largest = thought.id;
@@ -111,6 +57,12 @@ function submitThought(): void {
 		thought.render();
 	}
 	
+}
+
+function parseHtml(html: string): Element {
+	let div = document.createElement("div");
+	div.innerHTML = html;
+	return div.children[0];
 }
 
 function setStorage(): void {
